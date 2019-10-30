5422d937fe9233b19c018d46c9cf4bf488270e37
Laine Kendall <lainekendall@berkeley.edu>
2019-10-30 14:09:16 -0400

    Add hlint-test and apply-refact

--- test/EvaluatorSpec.hs
+++ test/EvaluatorSpec.hs
@@ -4,11 +4,11 @@ import Test.Hspec
 import Evaluator
 
 spec :: Spec
-spec = do
+spec =
   describe "eval" $ do
-    it "errors on Nothing" $ do
-      evaluate Nothing == "Error!"
-    it "errors when it can't finish" $ do
-      (evaluate $ Just (Value 1, "the rest")) == "Didn't finish parsing. So far: Value 1. Still left: the rest"
-    it "correctly evaluates expressions" $ do
-      (evaluate $ Just (MkAST Add (Value 1) (MkAST Subtract (Value 2) (MkAST Multiply (Value 4) (MkAST Divide (Value 5) (Value 10)))),"")) == "3"
+  it "errors on Nothing" $ do
+    evaluate Nothing == "Error!"
+  it "errors when it can't finish" $ do
+    (evaluate $ Just (Value 1, "the rest")) == "Didn't finish parsing. So far: Value 1. Still left: the rest"
+  it "correctly evaluates expressions" $ do
+    (evaluate $ Just (MkAST Add (Value 1) (MkAST Subtract (Value 2) (MkAST Multiply (Value 4) (MkAST Divide (Value 5) (Value 10)))),"")) == "3"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,2 @@
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+{-# OPTIONS_GHC -F -pgmF hlint-test #-}
