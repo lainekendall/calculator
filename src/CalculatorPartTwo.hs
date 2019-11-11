6a45238030fb6545255cad3d53fcb28f56329a5a
Laine Kendall <lainekendall@berkeley.edu>
2019-11-11 13:41:45 -0500

    Fix up calc part 2

--- src/CalculatorPartTwo.hs
+++ src/CalculatorPartTwo.hs
@@ -1,19 +1,24 @@
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE LambdaCase #-}
 
 module CalculatorPartTwo where
 
 import Data.Char
 import Data.List
+import qualified Data.Map as Map
 import ParserModel
 import Evaluator
 
---runParse :: Show a => String -> Parser a -> Maybe a
---runParse xs p = fmap (\(a, _) -> a) $ (runParser p) xs
---readUserInput :: IO String
---readUserInput = undefined
+parseExpression :: Parser AST
+parseExpression = undefined
+
+operatorsMap :: Map.Map Char Operator
+operatorsMap = Map.fromList [('+', Add),
+  ('-', Subtract),
+  ('*', Multiply),
+  ('/', Divide)]
 
 -- https://hackage.haskell.org/package/base-4.12.0.0/docs/Control-Applicative.html#t:Alternative
-
 many :: Parser a -> Parser [a]
 many f = some f <|> return []
 
@@ -31,21 +36,6 @@ satisfy f = parseChar >>= \c -> if f c
                                 then return c
                                 else Parser $ const Nothing
 
-parseChar :: Parser Char
-parseChar = Parser f
-  where
-    f "" = Nothing
-    f (c:cs) = Just (c, cs)
-
-parseNumber :: Parser Integer
-parseNumber = fmap read $ many $ satisfy isDigit
-
-parseValue :: Parser AST
-parseValue = fmap Value parseNumber
-
-parseSpace :: Parser String
-parseSpace = many $ satisfy isSpace
-
 char :: Char -> Parser Char
 char c = satisfy (==c)
 
@@ -56,70 +46,28 @@ string (c:cs) = do
   cs1 <- string cs
   return $ c1 : cs1
 
---parseOperator :: Parser (AST -> AST -> AST)
---parseOperator = Parser f
---  where
---    f "" = Nothing
---    f (c:cs) = case c of
---                 '+' -> Just ( Add, cs)
---                 '-' -> Just ( Subtract, cs)
---                 '*' -> Just ( Multiply, cs)
---                 '/' -> Just ( Divide, cs)
---
-----parseValue :: Parser AST
-----parseValue = undefined --mapP Value parseInt
-----
-----eval :: AST -> Integer
-----eval (Value i) = i
-----eval (Add a b) = eval a + eval b
-----eval (Subtract a b) = eval a - eval b
-----eval (Multiply a b) = eval a * eval b
-----eval (Divide a b) = eval a `div` eval b
-----
-----prettyPrint :: AST -> String
-----prettyPrint = undefined
-----
---data AST = Value Integer |  Add AST AST | Subtract AST AST | Multiply AST AST | Divide AST AST deriving Show
-----
-----testAST = Value 3
-----testAST1 = Add (Value 1) (Value 2)
-----
-----testInput = "3"
-----testInput1 = "1 + 2"
-----testInput2 = "1 - 3 + 4"
-----
----------------------------
------ Parsing Machinery ---
----------------------------
---newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
---
---instance Functor Parser where
---  fmap :: (a -> b) -> Parser a -> Parser b
---  fmap f (Parser p) = Parser $ \s -> case p s of
---                      Nothing -> Nothing
---                      Just (a, s1) -> Just (f a, s1)
---
---instance Applicative Parser where
---  pure :: a -> Parser a
---  pure a = Parser $ \s -> Just (a, s)
---  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
---  (<*>) (Parser pf) pa = Parser $ \s -> case pf s of
---                        Nothing -> Nothing
---                        Just (f, s1) -> runParser (fmap f pa) s1
---
---instance Monad Parser where
---  (>>=)  :: Parser a -> (a -> Parser b) -> Parser b
---  (>>=) (Parser pa) aToPb = Parser $ \s -> case pa s of
---    Nothing -> Nothing
---    Just (a, s1) -> runParser (aToPb a) s1
--- 
---fmpp :: Monad m => (a -> b) -> m a -> m b
---fmpp f ma = ma >>= \a -> return (f a)
---
---app :: Monad m => m (a -> b) -> m a -> m b
---app mf ma = mf >>= \f -> ma >>= \a -> return (f a)
---parsery' :: Parser String
---parsery' = do
---  c <- parseChar
---  c2 <- parseChar
---  pure [c,c2]
+parseChar :: Parser Char
+parseChar = Parser f
+  where
+    f "" = Nothing
+    f (c:cs) = Just (c, cs)
+
+parseNumber :: Parser Integer
+parseNumber = fmap read $ some $ satisfy isDigit
+
+parseValue :: Parser AST
+parseValue = fmap Value parseNumber
+
+parseSpace :: Parser String
+parseSpace = many $ satisfy isSpace
+
+parseMaybeOperator :: Parser (Maybe Operator)
+parseMaybeOperator = fmap (`Map.lookup` operatorsMap) parseChar
+
+parseOperator :: Parser Operator
+parseOperator = parseMaybeOperator >>= \case
+                                   Nothing -> Parser $ const Nothing
+                                   Just op -> return op
+choice :: [Parser a] -> Parser a
+choice [pa] = pa
+choice (pa:pas) = pa <|> choice pas
--- test/CalcPartTwoSpec.hs
+++ test/CalcPartTwoSpec.hs
@@ -10,23 +10,27 @@ import Test.QuickCheck
 
 spec :: Spec
 spec =
-  describe "CalculatorPartOne" $ do
+  describe "CalculatorPartTwo" $ do
     describe "parseChar" $ do
-      it "Empty" $
+      it "fails on empty" $
         isNothing $ runParser parseChar ""
       it "parses a char correctly" $ runParser parseChar "c" == Just ('c', "")
       it "only parses one char and leaves the rest" $ runParser parseChar "cab" == Just ('c', "ab")
-    describe "parseNumber" $ do
-      it "Empty" $ isNothing $ runParser parseNumber ""
-      it "parses a number" $ runParser parseNumber "123" == Just (123, "")
     describe "parseValue" $ do
-      it "Empty" $ isNothing $ runParser parseValue ""
-      it "parses a number into a AST Value" $ runParser parseValue "123" == Just (Value 123, "")
+      it "fails on empty" $ isNothing $ runParser parseValue ""
+      it "can't parse chars" $ isNothing $ runParser parseValue "cc"
+      it "parses a number into a AST Value" $ runParser parseValue "123cc1" == Just (Value 123,"cc1")
     describe "parseSpace" $ do
       it "Won't fail on empty" $ runParser parseSpace "" == Just ("", "")
       it "Doesn't parse numbers" $ runParser parseSpace "123" == Just ("", "123")
-      it "Parses multiple spaces" $ runParser parseSpace "       123  " == Just ("", "123  ")
+      it "Parses multiple spaces" $ runParser parseSpace "       123  " == Just ("       ","123  ")
     describe "string" $ do
       -- Add quick check for when s == s
-      it "Empty" $ isNothing $ runParser (string "") ""
-      it "Parses strings" $ runParser (string "Hello") "Hello! hi" == Just ("Hell", "! hi  h")
+      it "won't fail on empty" $ runParser (string "") "" == Just ("", "")
+      it "Parses strings" $ runParser (string "Hello") "Hello! hi" == Just ("Hello","! hi")
+    describe "parseOperator" $ do
+      it "fails on empty" $ isNothing $ runParser parseOperator ""
+      it "fails on other chars" $ isNothing $ runParser parseOperator "cc"
+      it "Parses + and leaves the rest" $ runParser parseOperator "+123" == Just (Add, "123")
+    describe "parseExpression" $
+      it "fails on empty" $ isNothing $ runParser parseExpression ""
