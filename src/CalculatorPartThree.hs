d166b0e23f5d07ffef55f1f3162b853db58cdec5
Laine Kendall <lainekendall@berkeley.edu>
2019-11-12 11:09:12 -0500

    Fix package, add calc three stuff

--- src/CalculatorPartThree.hs
+++ src/CalculatorPartThree.hs
@@ -1,7 +1,34 @@
 module CalculatorPartThree where
 
-import ParserModel
 import Evaluator
+import Text.Parsec
 
-parseExpression :: Parser AST
-parseExpression = undefined
+type Parser = Parsec String ()
+
+runParser :: Parsec String () a -> String -> Either ParseError a
+runParser p = parse p ""
+
+parseFullExpression :: Parser AST
+parseFullExpression = undefined
+
+parseExpression :: Parser Int
+parseExpression = do
+  spaces
+  i1 <- parseInteger
+  spaces
+  op <- parseOp
+  spaces
+  i2 <- 
+  spaces
+  return $ op i1 i2
+
+parseInteger :: Parser Int
+parseInteger = fmap read (many digit)
+
+parseOp :: Num a => Parser (a -> a -> a)
+parseOp = fmap opToFunc (oneOf "+-*") 
+
+opToFunc :: Num a => Char -> (a -> a -> a)
+opToFunc '+' = (+)
+opToFunc '-' = (-)
+opToFunc '*' = (*)
--- test/CalcPartThreeSpec.hs
+++ test/CalcPartThreeSpec.hs
@@ -3,7 +3,6 @@ module CalcPartThreeSpec where
 import CalculatorPartThree
 import Data.Maybe (isNothing)
 import Evaluator
-import ParserModel
 import Test.Hspec
 import Test.QuickCheck
 
@@ -11,9 +10,4 @@ spec :: Spec
 spec =
   describe "CalculatorPartThree" $ do
     describe "parseExpression" $ do
-      it "fails on empty" $ isNothing $ runParser parseExpression ""
-      it "fails on incomplete expressions" $ isNothing $ runParser parseExpression "1 + "
-      it "fails on chars" $ isNothing $ runParser parseExpression "cc2 + c"
-      it "parses values" $ runParser parseExpression "     1    " == Just (Value 1, "")
-      it "parses simple expressions" $ runParser parseExpression "1 - 2" == Just (MkAST Subtract (Value 1) (Value 2),"")
-      it "parses expressions" $ runParser parseExpression "     1    - 2 * 3+ 44 / 4" == Just (MkAST Subtract (Value 1) (MkAST Multiply (Value 2) (MkAST Add (Value 3) (MkAST Divide (Value 44) (Value 4)))),"")
+      it "fails on empty" $ runParser parseExpression "12 + 34" == Right 46
